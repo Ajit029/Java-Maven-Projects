c56b95d7237b6da1d678de436e4c2ebe02b82f80
Ajit029 <ajitsahoo540@gmail.com>
2023-04-24 14:35:11 +0530

    Half Triangle Pattern print

---
+++ pillar/src/main/java/basics/HalfTrianglePattern.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class HalfTrianglePattern {
+    public static void main(String[] args) {
+        halfTriangleUp();
+        halfTriangleDown();
+    }
+
+    public static void halfTriangleUp(){
+        for(int i = 1; i < 6; i++){
+            System.out.println("#".repeat(i));
+        }
+    }
+
+    public static void halfTriangleDown(){
+        for(int i = 5; i > 0; i--){
+            System.out.println("#".repeat(i));
+        }
+    }
+}
